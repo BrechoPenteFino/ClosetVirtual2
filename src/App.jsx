bca9dcf006883126fc1421aeb294553a226a269a
Lara Ivina <129535600+ivna0@users.noreply.github.com>
2023-11-24 14:47:18 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+import { useState } from 'react'
+import './App.css'
+import Home from '../src/assets/components/Home/Home'
+
+
+function App() {
+  return (
+    <Home/>
+  )
+}
+
+
+export default App
