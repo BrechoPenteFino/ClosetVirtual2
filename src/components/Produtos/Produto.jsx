b7c97c12592c1f3844a9992b31f77f893ef63332
Lara Ivina <129535600+ivna0@users.noreply.github.com>
2023-11-27 17:16:35 -0300

    Lara

---
+++ src/components/Produtos/Produto.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import style from './Produto.module.css';
+
+function Produto() {
+    return(
+        <>
+        <div className={style.container}>
+            <div className={style.cards}>
+
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default Produto;
